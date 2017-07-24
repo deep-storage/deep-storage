38922eaae77b589d599ea838bd10ee2dd30054f7
Jamie McCrindle <jamiemccrindle@gmail.com>
2017-07-24 22:48:03 +0100

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -11,20 +11,20 @@ export interface DeepStorage<State> extends DeepSubscriptions {
      * sets a value in deep storage by path and notifies subscribers. shortcut for
      * updateIn where the old value is ignored
      */
-    setIn: (...path: Path) => <DeepState>(newValue: DeepState) => void;
+    setIn: (...path: Path) => <DeepState>(newValue: DeepState) => Promise<DeepState>;
     /**
      * Updates the whole state and notifies subscribers
      */
-    update: (callback: (s: State) => State) => void;
+    update: (callback: (s: State) => State) => Promise<State>;
     /**
      * Updates a value in deep storage by path and notifies subscribers. Must not
      * mutate the oldValue
      */
-    updateIn: (...path: Path) => <DeepState>(callback: (s: DeepState) => DeepState) => void;
+    updateIn: (...path: Path) => <DeepState>(callback: (s: DeepState) => DeepState) => Promise<DeepState>;
     /**
      * Updates a property of the current state and notifies subscribers.
      */
-    updateProperty: <Key extends keyof State>(key: Key, callback: (s: State[Key]) => State[Key]) => void;
+    updateProperty: <Key extends keyof State>(key: Key, callback: (s: State[Key]) => State[Key]) => Promise<State[Key]>;
     /**
      * Returns the state that this deep storage is managing
      */
@@ -47,20 +47,18 @@ export interface DeepSubscription {
 }
 export declare type stringOrNumber = string | number;
 export declare type Path = stringOrNumber[];
-declare const _default: <State>(s: State) => DeepStorage<State>;
-export default _default;
 export declare class DefaultDeepStorage<State> implements DeepStorage<State> {
     state: State;
     private id;
     private subscriptions;
     constructor(state: State);
-    update: (callback: (s: State) => State) => void;
-    updateProperty: <Key extends keyof State>(key: Key, callback: (s: State[Key]) => State[Key]) => void;
-    setIn: (...path: (string | number)[]) => <DeepState>(newValue: DeepState) => void;
+    update: (callback: (s: State) => State) => Promise<State>;
+    updateProperty: <Key extends keyof State>(key: Key, callback: (s: State[Key]) => State[Key]) => Promise<State[Key]>;
+    setIn: (...path: (string | number)[]) => <DeepState>(newValue: DeepState) => Promise<DeepState>;
     merge: (partial: {
         [P in keyof State]?: State[P];
     }) => void;
-    updateIn: (...path: (string | number)[]) => <DeepState>(callback: (s: DeepState) => DeepState) => void;
+    updateIn: (...path: (string | number)[]) => <DeepState>(callback: (s: DeepState) => DeepState) => Promise<DeepState>;
     stateIn: <DeepState>(...path: (string | number)[]) => any;
     deep: <DeepState>(...path: (string | number)[]) => DeepStorage<DeepState>;
     subscription: (callback: StateUpdateCallback) => {
@@ -72,10 +70,10 @@ export declare class NestedDeepStorage<RootState, State> implements DeepStorage<
     path: Path;
     root: DeepStorage<RootState>;
     constructor(path: Path, root: DeepStorage<RootState>);
-    setIn: (...path: (string | number)[]) => <DeepState>(newValue: DeepState) => void;
-    update: (callback: (s: State) => State) => void;
-    updateIn: (...path: (string | number)[]) => <DeepState>(callback: (s: DeepState) => DeepState) => void;
-    updateProperty: <Key extends keyof State>(key: Key, callback: (s: State[Key]) => State[Key]) => void;
+    setIn: (...path: (string | number)[]) => <DeepState>(newValue: DeepState) => Promise<DeepState>;
+    update: (callback: (s: State) => State) => Promise<State>;
+    updateIn: (...path: (string | number)[]) => <DeepState>(callback: (s: DeepState) => DeepState) => Promise<DeepState>;
+    updateProperty: <Key extends keyof State>(key: Key, callback: (s: State[Key]) => State[Key]) => Promise<State[Key]>;
     readonly state: State;
     stateIn: <DeepState>(...path: (string | number)[]) => DeepState;
     deep: <DeepState>(...path: (string | number)[]) => DeepStorage<DeepState>;
@@ -87,3 +85,5 @@ export declare function parsePaths(paths: {
 }): {
     [key: string]: Path;
 };
+export declare const deepStorage: <State>(s: State) => DeepStorage<State>;
+export default deepStorage;
