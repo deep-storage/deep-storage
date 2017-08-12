c3356428546f4790d554c183e66d7bb35b205343
Jamie McCrindle <jamiemccrindle@gmail.com>
2017-08-12 12:05:30 +0100

    root

---
+++ async.d.ts
@@ -0,0 +1,44 @@
+import { DeepStorage, UsesDeepStorage } from "./index";
+export declare enum AsyncStatus {
+    Created = 0,
+    Running = 1,
+    Failed = 2,
+    Succeeded = 3,
+}
+export interface DeepAsyncData<Request, Response> {
+    status: AsyncStatus;
+    completed: boolean;
+    succeeded: boolean;
+    running: boolean;
+    started: boolean;
+    failed: boolean;
+    request?: Request;
+    response?: Response;
+    error?: any;
+}
+export interface DeepAsync<Request, Response> extends DeepAsyncData<Request, Response>, UsesDeepStorage<DeepAsyncData<Request, Response>> {
+    run(request: Request): Promise<DeepAsyncData<Request, Response>>;
+    rerun(): Promise<DeepAsyncData<Request, Response>>;
+    update(response: Response): Promise<DeepAsyncData<Request, Response>>;
+}
+export declare class AlreadyRunningError extends Error {
+}
+export declare class DefaultDeepAsync<Request, Response> implements DeepAsync<Request, Response> {
+    storage: DeepStorage<DeepAsyncData<Request, Response>>;
+    process: (request: Request) => Promise<Response>;
+    constructor(storage: DeepStorage<DeepAsyncData<Request, Response>>, process: (request: Request) => Promise<Response>);
+    run: (request: Request) => Promise<DeepAsyncData<Request, Response>>;
+    rerun: () => Promise<DeepAsyncData<Request, Response>>;
+    update: (response: Response) => Promise<DeepAsyncData<Request, Response>>;
+    readonly status: AsyncStatus;
+    readonly running: boolean;
+    readonly started: boolean;
+    readonly succeeded: boolean;
+    readonly failed: boolean;
+    readonly completed: boolean;
+    readonly request: Request;
+    readonly response: Response;
+    readonly error: any;
+}
+export declare const deepAsync: <Request, Response>(storage: DeepStorage<DeepAsyncData<Request, Response>, {}>, process: (request: Request) => Promise<Response>) => DefaultDeepAsync<Request, Response>;
+export default deepAsync;
